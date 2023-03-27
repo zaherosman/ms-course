28db806a529e15f4c1b2ba17a360c7bd53c3e816
Zaher Abou Osman <zaherabou2003@gmail.com>
2023-03-27 15:39:01 -0300

    Refactor Repository

---
+++ hr-payroll/src/main/java/com/zaher/hrpayroll/services/PaymentService.java
@@ -0,0 +1,21 @@
+package com.zaher.hrpayroll.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zaher.hrpayroll.entities.Payment;
+import com.zaher.hrpayroll.entities.Worker;
+import com.zaher.hrpayroll.feignclients.WorkerFeignClient;
+
+@Service
+public class PaymentService {
+	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
+	public Payment getPayment(long workerId, int days) {
+				
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+	}
+}
